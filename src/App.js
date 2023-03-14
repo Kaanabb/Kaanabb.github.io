4abe6f2ff608f7fe2e8809d62c581cfcebde6e25
kene40 <kabbasoglu@hotmail.com>
2023-03-14 03:16:50 +0300

    adds

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import {AiFillLinkedin} from "react-icons/ai";
 import {BsFillBriefcaseFill} from "react-icons/bs";
 import {RiBook2Fill} from "react-icons/ri";
 import './Main.css';
-// openai api key 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
+
 
 
 function App() {
@@ -50,9 +50,9 @@ function App() {
         <Row className="name-underpic">
             <h3>Kaan Abbasoğlu</h3>
             <p>Computer Engineer</p>
-           <a href='https://drive.google.com/file/d/12BW3DWHmX2uLbmOlONGY-OjKcsen0dNE/view?usp=share_link' target="blank" rel="noopener noreferrer">
+           <a href='https://drive.google.com/file/d/1S_o-FfaAbs3iMIe3U0a-YUy2ynpyAOS4/view?usp=sharing' target="blank" rel="noopener noreferrer">
             <Button type='submit' color='dark' >
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-person" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" fill="currentColor" class="bi bi-file-earmark-person" viewBox="0 0 16 16">
                 <path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                 <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2v9.255S12 12 8 12s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h5.5v2z"/>
                 </svg>
